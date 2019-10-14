22c41244595115afcd2a3c6ad860c8905252d8f9
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-14 14:58:38 -0600

    app2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      array: ['pasta', 'hot dogs', 'breakfast burritoes', 'potstickers']
+    }
+    
+  }
   render() {
+  const food = this.state.array.map((el,i)=> <h2 key={i}>{el}</h2>)
+    
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>App-2</h1>
+        {food}
       </div>
     );
   }
