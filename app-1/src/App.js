55562a4a7188c496ccb3d92dfe721813cb99cc64
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-14 14:41:11 -0600

    app1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+// import App1 from './components/App1'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      input: ''
+    }
+    // this.handleChange1 = this.handleChange1.bind(this)
+    // this.state.input = this.state.input.bind(this)
+  }
+
+  handleChange1(e){
+    this.setState({
+      input: e.target.value
+    })
+  }
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>App-1</h1>
+                <input onChange={(e) => this.handleChange1(e)}/>
+                <h2>{this.state.input}</h2>
+        <hr></hr>
       </div>
     );
   }
