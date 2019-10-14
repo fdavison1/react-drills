aeb44d8b1fa6be2ecbd93ad579246686d52a8d8b
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-14 15:27:19 -0600

    app3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      array: ['pasta', 'hot dogs', 'breakfast burritoes', 'potstickers'],
+      array2: ""
+    }
+  }
+
+  handleChange2(e){
+    this.setState({
+      array2: e 
+    })
+  }
+
   render() {
+    // const food = this.state.array.map((el,i) => <h2 key={i}>{el}</h2>)
+    let food = this.state.array
+    .filter((el)=> {
+      return el.includes(this.state.array2)
+    })
+    .map((el, i)=> {
+      return <h2 key={i}>{el}</h2>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>App-3</h1>
+        <input onChange={(e)=> this.handleChange2(e.target.value)} type='text'/>
+        {food}
       </div>
     );
   }
